8ec658c97287e7e084423b1ab5fb5252604175be
Abhay Vishwakarma <gaamaofficial@gmail.com>
2026-06-08 21:43:25 +0530

    Scroll was not working. Now its fixed

--- tailor-mart-user-side/src/Pages/Feed/Feed.jsx
+++ tailor-mart-user-side/src/Pages/Feed/Feed.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../../Components/All-size-Navbar/Navbar"
-
+import './Feed.css'
 function Feed() {
   return (
     <div className="feed-main">
@@ -7,10 +7,11 @@ function Feed() {
         <Navbar/>
       </div>
       <div className="feed-cont">
-        <div>
-            <img src="https://images.unsplash.com/photo-1773332598413-a6d5279d1ae8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <img src="https://images.unsplash.com/photo-1773332598413-a6d5279d1ae8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        </div>
+        <div className="h-96 bg-red-400">uii</div>
+        <div className="h-96 bg-red-400">uii</div>
+        <div className="h-96 bg-red-400">uii</div>
+        <div className="h-96 bg-red-400">uii</div>
+        <div className="h-96 bg-red-400">uii</div>
       </div>
     </div>
   )
