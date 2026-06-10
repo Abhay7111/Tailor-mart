e9a86c47f67ff59899e2baec5cff1c288c2b7735
Abhay Vishwakarma <gaamaofficial@gmail.com>
2026-06-10 18:03:08 +0530

    Add search component and common styles
    Introduce a Search component (searchProduct.jsx) with accompanying SCSS/CSS and source map, and add shared commonStyle (SCSS/CSS and source map) for utility classes. Integrate Poppins font in index.html and import commonStyle.css and SearchProduct into Feed.jsx; update Feed layout to include the search bar and a section heading. Also add horizontal padding tweaks to Feed CSS/SCSS.

---
+++ tailor-mart-user-side/src/Components/Search/searchProduct.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+function searchProduct() {
+  return (
+    <div className="search-main">
+        <i className='ri-search-2-line'></i>
+        <input type="search" name="products" id="products" placeholder="Fashion"/>
+    </div>
+  )
+}
+
+export default searchProduct
--- tailor-mart-user-side/src/Pages/Feed/Feed.jsx
+++ tailor-mart-user-side/src/Pages/Feed/Feed.jsx
@@ -1,6 +1,7 @@
-import Navbar from "../../Components/All-size-Navbar/Navbar"
-import Popular_random from "../../Components/RandomProducts/Popular.random"
-
+import '../../Styles/commonStyle.css'
+import Navbar from "../../Components/All-size-Navbar/Navbar";
+import Popular_random from "../../Components/RandomProducts/Popular.random";
+import SearchProduct from "../../Components/Search/searchProduct"
 
 function Feed() {
 
@@ -9,8 +10,13 @@ function Feed() {
       <div className="feed-top-navbar">
         <Navbar/>
       </div>
-      <div className="feed-search-bar"></div>
-        <Popular_random/>
+      <div className="feed-search-bar">
+       <SearchProduct /> 
+      </div>
+        <div className="commonStyle-flex-col-start gap-2.5">
+          <h1 className="commonStyle-h1">Our most favoret</h1>
+          <Popular_random/>
+        </div>
     </div>
   )
 }
