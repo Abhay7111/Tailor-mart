b845b9922906eae671f0a3884748982a3681a0b1
Abhay Vishwakarma <gaamaofficial@gmail.com>
2026-06-14 14:38:31 +0530

    Remove backend directory
    Delete the entire backend service and related files (app.js, controllers/user.controller.js, models/user.model.js, routes/user.route.js, utils/Database.js, package.json, .gitignore). This removes the server implementation and associated backend configuration from the repository.

--- backend/routes/user.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const userRoute = express.Router();
-const userController = require("../controllers/user.controller");
-
-userRoute.post("/register", userController.registerUser);
-
-module.exports = userRoute;
